ca6b290c71ee8027cf0baf2c717f5c73080a804e
Andrey <Glukmann@users.noreply.github.com>
2017-11-09 11:52:41 +0500

    1.3 debug

--- crecon/urls.py
+++ crecon/urls.py
@@ -1,17 +1,13 @@
 # -*- coding: utf-8 -*-
-from django.conf import settings
 from django.conf.urls import include, url
 from . import views
 
 from django.contrib import admin
 
-from filebrowser.sites import site
-
 admin.autodiscover()
 
 urlpatterns =  [
     url(r'^$', views.home, name='home'),
-    url(r'^admin/filebrowser/', include(site.urls)),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^saleslist/', views.saleslist, name='saleslist'),
     url(r'^prognoz/', views.prognoz, name='prognoz'),
