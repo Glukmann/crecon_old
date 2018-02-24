abb802373f6fae9ec7c014414b1cb8b1e9b179fd
Andrey <Glukmann@users.noreply.github.com>
2018-02-24 18:01:55 +0500

    1.4.3 check boxes choice

--- crecon/views.py
+++ crecon/views.py
@@ -36,15 +36,12 @@ def prognoz(request):
         if form.is_valid():
             for key, val in form.cleaned_data.items():
                 setattr(metods, key, val)
-            try:
                 shop = form.data['mag']
                 g = Graph()
                 table_sales = Salestring.objects.filter(title=shop)
                 context = g.universal_prognoz(table_sales, metods)
 
                 return render(request, 'prognoz.html', {'table_sales': table_sales, 'graph': context})
-            except:
-                raise Http404("No shop matches the given query.")
     else:
         raise Http404("No MyModel matches the given query.")
 
